39e97d71130d8fb7b0c9d67809c68fdf81dfcc93
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-03-08 01:16:54 -0800

    database connected

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { connectDB } = require('./config/db')
 const port = process.env.PORT
+
+connectDB()
 
 const app = express()
 
+// ROUTES
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.listen(port, () => {
-  console.log(`server on port ${port}`)
-})
-
 app.use('*', (req, res) => {
   res.status(404).json({ error: 'not found' })
+})
+
+app.listen(port, () => {
+  console.log(`server on port ${port}`)
 })
