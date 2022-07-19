559200b3d86f7041cca98350487712ad039f6939
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-19 14:22:19 -0700

    feat(schemas): add complete property to goal and task schemas

--- server/models/goalModel.js
+++ server/models/goalModel.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const goalSchema = Schema(
   {
     user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
     title: { type: String, required: true },
+    complete: { type: Boolean, default: false, required: true },
   },
   { timestamps: true }
-)
+);
 
-module.exports = mongoose.model('Goal', goalSchema)
+module.exports = mongoose.model('Goal', goalSchema);
--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const taskSchema = Schema(
   {
     user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
     title: { type: String, required: [true, 'Please name your task'] },
+    complete: { type: Boolean, default: false, required: true },
     goal: { type: Schema.Types.ObjectId, ref: 'Goal' },
   },
   { timestamps: true }
-)
+);
 
-module.exports = mongoose.model('Task', taskSchema)
+module.exports = mongoose.model('Task', taskSchema);
