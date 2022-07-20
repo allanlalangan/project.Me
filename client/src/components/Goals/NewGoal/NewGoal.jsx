737e878768fb05d369696d45d6db2d7299b7ad92
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-19 20:26:44 -0700

    feat: update NewGoal styles

--- client/src/components/Goals/NewGoal/NewGoal.jsx
+++ client/src/components/Goals/NewGoal/NewGoal.jsx
@@ -12,7 +12,7 @@ const NewGoal = ({ gridItem }) => {
   const { handleAddGoal, handleAddGoalTextChange, addGoalText } = useNewGoal();
   return (
     <article className={`${gridItem} ${styles['container']}`}>
-      <h3>Set a new goal</h3>
+      <h3 className={styles.heading}>Set a new goal</h3>
       <form onSubmit={handleAddGoal} className={styles['new-goal__form']}>
         <fieldset className={styles['new-goal__fieldset']}>
           <label htmlFor='title'></label>
