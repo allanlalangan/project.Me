b192c06d6601d74db383208ea0d1a8cdb231edac
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-26 10:58:16 -0700

    update axios routes

--- client/src/features/authSlice.js
+++ client/src/features/authSlice.js
@@ -1,8 +1,8 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 //Get user from localStorage if it exists
-const existingUser = JSON.parse(localStorage.getItem('user'))
+const existingUser = JSON.parse(localStorage.getItem('user'));
 
 const initialState = {
   user: existingUser ? existingUser : null,
@@ -10,97 +10,103 @@ const initialState = {
   isError: false,
   isSuccess: false,
   message: '',
-}
+};
 
 const register = createAsyncThunk(
   'auth/register',
   async (formData, thunkAPI) => {
     try {
-      const response = await axios.post('/api/users', formData)
+      const response = await axios.post(
+        'https://projectme-srv.herokuapp.com/api/users',
+        formData
+      );
       if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
+        localStorage.setItem('user', JSON.stringify(response.data));
       }
-      return response.data
+      return response.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.toString())
+      return thunkAPI.rejectWithValue(error.toString());
     }
   }
-)
+);
 
 const login = createAsyncThunk('auth/login', async (formData, thunkAPI) => {
   try {
-    const response = await axios.post('/api/users/login', formData)
+    const response = await axios.post(
+      'https://projectme-srv.herokuapp.com/api/users/login',
+      formData
+    );
     if (response.data) {
-      localStorage.setItem('user', JSON.stringify(response.data))
+      localStorage.setItem('user', JSON.stringify(response.data));
     }
-    return response.data
+    return response.data;
   } catch (error) {
-    return thunkAPI.rejectWithValue(error.toString())
+    return thunkAPI.rejectWithValue(error.toString());
   }
-})
+});
 
 const logout = createAsyncThunk('auth/logout', async (thunkAPI) => {
   try {
-    await localStorage.removeItem('user')
+    await localStorage.removeItem('user');
   } catch (error) {
-    return thunkAPI.rejectWithValue(error.toString())
+    return thunkAPI.rejectWithValue(error.toString());
   }
-})
+});
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     reset(state) {
-      state.user = null
-      state.isLoading = false
-      state.isError = false
-      state.isSuccess = false
-      state.message = ''
+      state.user = null;
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = '';
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(register.pending, (state) => {
-        state.message = 'Registering User'
-        state.isLoading = true
+        state.message = 'Registering User';
+        state.isLoading = true;
       })
       .addCase(register.fulfilled, (state, action) => {
         state.message =
-          'Successfully registered new user. A verification email has been sent. Please verify your account.'
-        state.user = action.payload
-        state.isLoading = false
-        state.isSuccess = true
+          'Successfully registered new user. A verification email has been sent. Please verify your account.';
+        state.user = action.payload;
+        state.isLoading = false;
+        state.isSuccess = true;
       })
       .addCase(register.rejected, (state, action) => {
-        state.user = null
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
+        state.user = null;
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
       })
       .addCase(login.pending, (state) => {
-        state.message = 'Logging in...'
-        state.isLoading = true
+        state.message = 'Logging in...';
+        state.isLoading = true;
       })
       .addCase(login.fulfilled, (state, action) => {
-        state.message = ''
-        state.user = action.payload
-        state.isLoading = false
-        state.isSuccess = true
+        state.message = '';
+        state.user = action.payload;
+        state.isLoading = false;
+        state.isSuccess = true;
       })
       .addCase(login.rejected, (state, action) => {
-        state.user = null
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
+        state.user = null;
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
       })
       .addCase(logout.fulfilled, (state) => {
-        state.message = 'User successfully logged out'
-        state.user = null
-      })
+        state.message = 'User successfully logged out';
+        state.user = null;
+      });
   },
-})
+});
 
-export default authSlice.reducer
-export const { reset } = authSlice.actions
-export { register, login, logout }
+export default authSlice.reducer;
+export const { reset } = authSlice.actions;
+export { register, login, logout };
