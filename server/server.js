9a88b317380f7f8350e2645fa83ecebb7bcca610
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-26 10:49:00 -0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const { connectDB } = require('./config/db');
 
 // SERVER START
 const app = express();
 const port = process.env.PORT;
-app.listen(port || 5000, () => {
-  console.log(`server on port ${port}`);
-});
-
-// CONNECT TO DATABASE
-connectDB();
-
+// app.listen(port || 5000, () => {
+//   console.log(`server on port ${port}`);
+// });
+app.use(cors());
 // PARSE BODY MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// CONNECT TO DATABASE
+connectDB();
 
 // ROUTES
 app.use('/api/goals', require('./routes/goalsRoutes'));
