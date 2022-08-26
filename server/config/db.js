648708f76aa905ff0e459d631034af45c3705a54
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-26 10:48:02 -0700

    npm i cors

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.DB_URI ||
-        'mongodb+srv://allanladev:mernstack@dashboard.3txgk.mongodb.net/dashboard?authSource=admin&replicaSet=atlas-w1no9n-shard-0&readPreference=primary&ssl=true'
+      'mongodb+srv://allanladev:mernstack@dashboard.3txgk.mongodb.net/dashboard?authSource=admin&replicaSet=atlas-w1no9n-shard-0&readPreference=primary&ssl=true',
+      { useNewUrlParser: true }
     );
     console.log('database connected');
   } catch (error) {
