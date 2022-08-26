a0bf8150d68a6a1b71a2372e6a22b2419a157826
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-08-26 09:53:49 -0700

    fix: update api routes in goalsSlice

--- client/src/features/goalsSlice.js
+++ client/src/features/goalsSlice.js
@@ -18,10 +18,7 @@ const getGoals = createAsyncThunk('goals/getGoals', async (_, thunkAPI) => {
       },
     };
 
-    const resp = await axios.get(
-      'https://projectme-server.herokuapp.com/api/goals',
-      config
-    );
+    const resp = await axios.get('/api/goals', config);
     return resp.data;
   } catch (error) {
     console.log('ERROR GETGOALS getGoals asyncThunk throw error');
@@ -44,11 +41,7 @@ const addGoal = createAsyncThunk(
         },
       };
 
-      const resp = await axios.post(
-        'https://projectme-server.herokuapp.com/api/goals',
-        formData,
-        config
-      );
+      const resp = await axios.post('/api/goals', formData, config);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.toString());
@@ -65,10 +58,7 @@ const updateGoal = createAsyncThunk('goals/delete', async (id, thunkAPI) => {
       },
     };
 
-    const resp = await axios.put(
-      `https://projectme-server.herokuapp.com/api/goals/${id}`,
-      config
-    );
+    const resp = await axios.put(`/api/goals/${id}`, config);
     return resp.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.toString());
@@ -84,10 +74,7 @@ const deleteGoal = createAsyncThunk('goals/delete', async (id, thunkAPI) => {
       },
     };
 
-    const resp = await axios.delete(
-      `https://projectme-server.herokuapp.com/api/goals/${id}`,
-      config
-    );
+    const resp = await axios.delete(`/api/goals/${id}`, config);
     console.log(resp.data);
     return resp.data;
   } catch (error) {
